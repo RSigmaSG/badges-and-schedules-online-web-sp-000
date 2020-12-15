09ed10c2a4a782e0a9dd34bbdbd65bce388b9ac0
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-15 02:44:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,7 +26,7 @@ def assign_rooms(array)
   out_arr = []
   array.each_with_index do |name, index|
     
-    out_arr.push("Hello, #{name}! You'll be assigned to room _____!")
+    out_arr.push("Hello, #{name}! You'll be assigned to room #{index}!")
     
   end
   
